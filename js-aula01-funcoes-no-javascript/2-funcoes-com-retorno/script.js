ae24d1bc62c4ba1e415319f5e27c6473eafa4765
Luiz Cubas <lhs.cubas@gmail.com>
2026-08-07 00:28:52 -0300

    segunda prática de javascript

---
+++ js-aula01-funcoes-no-javascript/2-funcoes-com-retorno/script.js
@@ -0,0 +1,26 @@
+// funções com retorno
+
+const n1 = 50;
+const n2 = 10;
+
+// Essa função gera um resultado e exporta
+function multiplicar(n1,n2){
+    return n1 * n2;
+}
+
+// Exibindo o resultado de uma função
+document.write(`<p>O resultado da multiplicação de ${n1} por ${n2} é igual a <strong>${multiplicar(n1, n2)}</strong>.</p>`)
+
+const = number = 4;
+
+function parImpar(number){
+    if (number % 2 === 0){
+        return "par";
+    } else {
+        return "Impar";
+    }
+}
+
+document.write(
+    `<p>O número ${number} é ${parImpar(number)}</p>`
+)
